01932afb69d62d6e1712ec68927d1f608652e73e
LenaNguyen27 <nguyen.lena27@hotmail.com>
2019-08-23 21:06:51 -0400

    set up camera screen

--- app/modules/CameraSection.js
+++ app/modules/CameraSection.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, SafeAreaView, TouchableOpacity, Image, StyleSheet } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import { Camera } from 'expo-camera';
+import { Button, Header, Footer, Icon, Right } from 'native-base';
 
-export default class CameraExample extends React.Component {
+export default class CameraSection extends React.Component {
 	state = {
 		hasCameraPermission: null,
 		type: Camera.Constants.Type.back,
@@ -14,42 +15,80 @@ export default class CameraExample extends React.Component {
 		this.setState({ hasCameraPermission: status === 'granted' });
 	}
 
+	flipCamera = () => {
+		this.setState({
+			type:
+				this.state.type === Camera.Constants.Type.back
+					? Camera.Constants.Type.front
+					: Camera.Constants.Type.back,
+		});
+	}
+
+	takePhoto = async () => {
+		console.log('Button Pressed');
+		if (this.camera) {
+			console.log('Taking photo');
+			const options = {
+				quality: 1, base64: true, fixOrientation: true,
+				exif: true
+			};
+			let photo = await this.camera.takePictureAsync(options);
+			console.log(photo);
+		}
+	}
+
 	render() {
 		const { hasCameraPermission } = this.state;
 		if (hasCameraPermission === null) {
-			return <View />;
+			return <SafeAreaView />;
 		} else if (hasCameraPermission === false) {
 			return <Text>No access to camera</Text>;
 		} else {
 			return (
-				<View style={{ flex: 1 }}>
-					<Camera style={{ flex: 1 }} type={this.state.type}>
-						<View
-							style={{
-								flex: 1,
-								backgroundColor: 'transparent',
-								flexDirection: 'row',
-							}}>
-							<TouchableOpacity
-								style={{
-									flex: 0.1,
-									alignSelf: 'flex-end',
-									alignItems: 'center',
-								}}
-								onPress={() => {
-									this.setState({
-										type:
-											this.state.type === Camera.Constants.Type.back
-												? Camera.Constants.Type.front
-												: Camera.Constants.Type.back,
-									});
-								}}>
-								<Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
-							</TouchableOpacity>
-						</View>
+				<SafeAreaView style={{ flex: 1 }}>
+					<Camera
+						style={{ flex: 1, justifyContent: 'space-between' }}
+						type={this.state.type}
+						ref={ref => {
+							this.camera = ref;
+						}}>
+						<Header transparent >
+							<Right>
+								<Button transparent onPress={this.flipCamera}>
+									<Icon type="Ionicons" name="reverse-camera" style={{ fontSize: 30 }} />
+								</Button>
+							</Right>
+						</ Header>
+						<Button style={styles.footer} onPress={this.takePhoto}>
+							<Icon type="Ionicons" name="radio-button-on" style={{ fontSize: 50 }} />
+						</Button>
 					</Camera>
-				</View>
+				</SafeAreaView>
 			);
 		}
 	}
 }
+
+const styles = StyleSheet.create({
+	captureButton: {
+		alignSelf: 'flex-end',
+		alignItems: 'center',
+		backgroundColor: 'purple'
+	},
+	footer: {
+		backgroundColor: 'transparent',
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignSelf: 'center'
+	},
+	header: {
+		position: 'absolute',
+		backgroundColor: 'transparent',
+		left: 0,
+		top: 0,
+		right: 0,
+		zIndex: 100,
+		alignItems: 'center'
+	}
+
+})
