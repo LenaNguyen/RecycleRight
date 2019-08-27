5681e71e4c47215484ca6cec7f65b3fe1bd1d5fe
LenaNguyen27 <nguyen.lena27@hotmail.com>
2019-08-27 15:26:01 -0400

    finished the camera section and submit photo

--- App.js
+++ App.js
@@ -1,15 +1,121 @@
 import React from 'react';
-import { Text } from 'react-native';
-import CameraExample from './app/modules/CameraSection';
+import { StyleSheet, ActivityIndicator, View, ImageBackground, TouchableOpacity, SafeAreaView, Text } from 'react-native';
+import CameraSection from './app/modules/CameraSection';
 import Gallery from './app/components/Gallery';
+import { Icon } from 'native-base';
+import { Ionicons } from '@expo/vector-icons';
 
 export default class App extends React.Component {
+	state = {
+		loading: false
+	}
+	classifyPhoto = async (photo) => {
+		this.handleLoading();
+		this.setState({ photoUri: photo.uri });
+		console.log('photo received');
+		try {
+			const uriParts = photo.uri.split('.');
+			const fileType = uriParts[uriParts.length - 1];
+			let formData = new FormData();
+			formData.append('imageFile', {
+				uri: photo.uri,
+				name: `photo.${fileType}`,
+				type: `image/${fileType}`
+			});
+
+			let options = {
+				method: 'POST',
+				body: formData,
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'multipart/form-data',
+				},
+			};
+
+			console.log('sending');
+			const resp = await fetch('https://morning-hamlet-65403.herokuapp.com/api/waste', options);
+			const { group, score, message } = await resp.json();
+			this.setState({ group, score, message })
+			console.log(group);
+			this.handleLoading();
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	handleClose = () => {
+		this.setState({ photoUri: null });
+	}
+
+	handleLoading = () => {
+		const { loading } = this.state;
+		this.setState({ loading: !loading })
+	}
+
 	render() {
+		const { loading, group, score, message } = this.state;
+		if (loading)
+			return (
+				<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+					<ActivityIndicator size='large' color="#00ff00" />
+				</View>
+			)
+		else if (this.state.photoUri) {
+			return (
+				<ImageBackground source={{ uri: this.state.photoUri }} style={styles.container} >
+					<SafeAreaView style={styles.header}>
+						<TouchableOpacity onPress={this.handleClose} style={{ minHeight: 40, minWidth: 40 }}>
+							<Ionicons name="md-close-circle-outline" size={30} color="white" />
+						</TouchableOpacity>
+					</SafeAreaView>
+					<View style={styles.infoContainer} >
+						<View style={styles.infoBox}>
+							<Text style={styles.bold}> Group:
+								<Text>{group}</Text>
+							</Text>
+							<Text style={styles.bold}> Score:
+								<Text>{score}</Text>
+							</Text>
+						</View>
+					</View>
+				</ImageBackground>
+			)
+		}
 		return (
 			<React.Fragment>
-				<Gallery />
-				{/* <CameraExample /> */}
+				<CameraSection onPhotoTaken={this.classifyPhoto} onLoad={this.handleLoading} />
 			</React.Fragment>
 		);
 	}
 }
+
+styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+	infoContainer: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		paddingBottom: 100
+	},
+	infoBox: {
+		width: 200,
+		height: 200,
+		backgroundColor: 'white',
+		opacity: 0.6,
+		zindex: 50,
+		justifyContent: 'center',
+		alignItems: 'center',
+		padding: 10
+	},
+	header: {
+		flex: 0.18,
+		alignItems: 'flex-end',
+		justifyContent: 'center',
+		zindex: 100,
+	},
+	bold: {
+		fontWeight: 'bold'
+	}
+}) 
--- app/modules/CameraSection.js
+++ app/modules/CameraSection.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Text, SafeAreaView, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import { Text, SafeAreaView, TouchableOpacity, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import * as Permissions from 'expo-permissions';
 import { Camera } from 'expo-camera';
-import { Button, Header, Footer, Icon, Right } from 'native-base';
+import { Button, Header, Icon, Right } from 'native-base';
 
 export default class CameraSection extends React.Component {
 	state = {
@@ -25,20 +26,29 @@ export default class CameraSection extends React.Component {
 	}
 
 	takePhoto = async () => {
-		console.log('Button Pressed');
-		if (this.camera) {
-			console.log('Taking photo');
-			const options = {
-				quality: 1, base64: true, fixOrientation: true,
-				exif: true
-			};
-			let photo = await this.camera.takePictureAsync(options);
-			console.log(photo);
+		try {
+			const { onPhotoTaken, onLoad } = this.props;
+			//onLoad();
+			if (this.camera) {
+				this.setState({ disabled: true })
+				console.log('taking photo');
+				const options = {
+					quality: 1,
+					base64: true,
+					fixOrientation: true,
+					exif: true
+				}
+				let photo = await this.camera.takePictureAsync();
+				onPhotoTaken(photo);
+				console.log(photo);
+			}
+		} catch (error) {
+			console.log(error)
 		}
 	}
 
 	render() {
-		const { hasCameraPermission } = this.state;
+		const { hasCameraPermission, disabled } = this.state;
 		if (hasCameraPermission === null) {
 			return <SafeAreaView />;
 		} else if (hasCameraPermission === false) {
@@ -59,9 +69,9 @@ export default class CameraSection extends React.Component {
 								</Button>
 							</Right>
 						</ Header>
-						<Button style={styles.footer} onPress={this.takePhoto}>
-							<Icon type="Ionicons" name="radio-button-on" style={{ fontSize: 50 }} />
-						</Button>
+						<TouchableOpacity style={[styles.footer, disabled ? { opacity: 0.2 } : null]} onPress={this.takePhoto} disabled={disabled}>
+							<Ionicons name="md-radio-button-on" size={60} color="white" />
+						</TouchableOpacity>
 					</Camera>
 				</SafeAreaView>
 			);
@@ -79,7 +89,9 @@ const styles = StyleSheet.create({
 		backgroundColor: 'transparent',
 		flexDirection: 'row',
 		justifyContent: 'center',
-		alignSelf: 'center'
+		alignSelf: 'center',
+		padding: 15,
+		zIndex: 100
 	},
 	header: {
 		position: 'absolute',
